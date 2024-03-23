3b50dca2b7c0ada3c94c7ea436603127b8a2efbf
JoaoVitorDiass <joovitordiasdasilva@gmail.com>
2024-03-22 22:48:55 -0300

    fix: ajustes aula 2203

--- src/controls/ControlVenda.php
+++ src/controls/ControlVenda.php
@@ -5,6 +5,7 @@
     require_once "../../vendor/autoload.php";
     
     use Comercio\Api\models\Venda;
+    use Comercio\Api\models\Cliente;
     use Comercio\Api\models\ItemVenda;
     use Comercio\Api\utils\SingletonConexao;
     use Comercio\Api\utils\Funcoes;
@@ -134,96 +135,100 @@
         header('Content-Type: Application/json');
         return json_encode($retorno);
     }
-    // function Adicionar($data) {
-    //     $retorno = Funcoes::getRetorno();
+    function Adicionar($data) {
+        $retorno = Funcoes::getRetorno();
+        try {
+            $venda = new Venda(
+                0,
+                new Cliente($data["codigoCliente"]),
+            );
 
-    //     try {
-    //         $fornecedor = new Fornecedor(
-    //             0,
-    //             $data["nome"],
-    //             $data["cnpj"],
-    //             $data["telefone"],
-    //             $data["email"],
-    //             $data["endereco"],
-    //         );
+            $conexao = SingletonConexao::getInstancia();
+            $success = $venda->Adicionar($conexao);
+            SingletonConexao::getInstancia()->fecharConexao();
 
-    //         $conexao = Singleton::getConexao();
-    //         $success = $fornecedor->Adicionar($conexao);
-    //         Singleton::fecharConexao();
+            foreach($data["items"] as $item) {
+                $itemVenda = new ItemVenda(
+                    0,
+                    $data["codigoCliente"],
+                    $item["codigoProduto"],
+                    $item["quantidade"],
+                );
+                $conexao = SingletonConexao::getInstancia();
+                $success = $itemVenda->Adicionar($conexao);
+                SingletonConexao::getInstancia()->fecharConexao();
+            }
 
-    //         if($success) {
-    //             http_response_code(201);
-    //         }
-    //         else {
-    //             throw new Exception("Erro ao adicionar o produto ...");
-    //         }
-    //     }
-    //     catch (Exception $e)
-    //     {
-    //         $retorno['error'] = true;
-    //         $retorno['mensage'][] = $e->getMessage();
-    //         http_response_code(400);
-    //     }
-    //     header('Content-Type: Application/json');
-    //     return json_encode($retorno);
-    // }
-    // function Alterar($data) {
-    //     $retorno = Funcoes::getRetorno();
-    //     try {
-    //         $fornecedor = new Fornecedor(
-    //             $data["codigo"],
-    //             $data["nome"],
-    //             $data["cnpj"],
-    //             $data["telefone"],
-    //             $data["email"],
-    //             $data["endereco"],
-    //         );
+            if($success) {
+                http_response_code(201);
+            }
+            else {
+                throw new Exception("Erro ao adicionar a venda ...");
+            }
+        }
+        catch (Exception $e)
+        {
+            $retorno['error'] = true;
+            $retorno['mensage'][] = $e->getMessage();
+            http_response_code(400);
+        }
+        header('Content-Type: Application/json');
+        return json_encode($retorno);
+    }
+    function Alterar($data) {
+        $retorno = Funcoes::getRetorno();
+        try {
+            $venda = new Venda(
+                $data["codigo"],
+                new Cliente($data["codigoCliente"]),
+                $data["valorTotal"],
+            );
 
-    //         $conexao = Singleton::getConexao();
-    //         $success = $fornecedor->Alterar($conexao);
-    //         Singleton::fecharConexao();
+            $conexao = SingletonConexao::getInstancia();
+            $success = $venda->Alterar($conexao);
+            SingletonConexao::getInstancia()->fecharConexao();
 
-    //         if($success) {
-    //             http_response_code(202);
-    //         }
-    //         else {
-    //             throw new Exception("Erro ao alterar o produto ...");
-    //         }
-    //     }
-    //     catch (Exception $e)
-    //     {
-    //         $retorno['error'] = true;
-    //         $retorno['mensage'][] = $e->getMessage();
-    //         http_response_code(400);
-    //     }
-    //     header('Content-Type: Application/json');
-    //     return json_encode($retorno);
-    // }
-    // function Deletar($codigoFornecedor) {
-    //     $retorno = Funcoes::getRetorno();
-    //     try {
-    //         $fornecedor = new Fornecedor();
+            if($success) {
+                http_response_code(202);
+            }
+            else {
+                throw new Exception("Erro ao alterar o produto ...");
+            }
+        }
+        catch (Exception $e)
+        {
+            $retorno['error'] = true;
+            $retorno['mensage'][] = $e->getMessage();
+            http_response_code(400);
+        }
+        header('Content-Type: Application/json');
+        return json_encode($retorno);
+    }
+    function Deletar($codigoVenda) {
+        $retorno = Funcoes::getRetorno();
+        try {
+            $venda = new Venda();
 
-    //         $conexao = Singleton::getConexao();
-    //         $success = $fornecedor->Deletar($codigoFornecedor, $conexao);
-    //         Singleton::fecharConexao();
+            $conexao = SingletonConexao::getInstancia();
+            $success = $venda->Deletar($codigoVenda, $conexao);
+            SingletonConexao::getInstancia()->fecharConexao();
             
-    //         if($success) {
-    //             http_response_code(200);
-    //         }
-    //         else {
-    //             throw new Exception("Erro ao alterar o usuário ...");
-    //         }
-    //     }
-    //     catch (Exception $e)
-    //     {
-    //         $retorno['error'] = true;
-    //         $retorno['mensage'][] = $e->getMessage();
-    //         http_response_code(400);
-    //     }
-    //     header('Content-Type: Application/json');
-    //     return json_encode($retorno);
-    // }
+            if($success) {
+                http_response_code(200);
+            }
+            else {
+                throw new Exception("Erro ao alterar o usuário ...");
+            }
+        }
+        catch (Exception $e)
+        {
+            $retorno['error'] = true;
+            $retorno['mensage'][] = $e->getMessage();
+            http_response_code(400);
+        }
+        header('Content-Type: Application/json');
+        return json_encode($retorno);
+    }
 
     switch($_SERVER["REQUEST_METHOD"]){
         case "GET":
@@ -239,9 +244,9 @@
             }
         break;
 
-        // case "POST":
-        //     echo Adicionar(Funcoes::getData());
-        // break;
+        case "POST":
+            echo Adicionar(Funcoes::getData());
+        break;
 
         // case "DELETE":
         //     echo Deletar($_GET["codigo"]);
